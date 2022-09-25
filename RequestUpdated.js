069737116b9df9ff431e54491c0d7f4b444e012e
Amelia <53657436+AmeliaTYR@users.noreply.github.com>
2022-09-25 10:47:06 +0800

    Update RequestUpdated.js

--- RequestUpdated.js
+++ RequestUpdated.js
@@ -1,7 +1,16 @@
+import wixWindow from 'wix-window';
+import wixData from 'wix-data';
+import wixLocation from 'wix-location';
+
+var currentItem = null;
+
 $w.onReady(function () {
+    let received = wixWindow.lightbox.getContext();
+    currentItem = received.currentItem
+    $w('#itemText').text = "Your request for " + currentItem.shortAnswerField2 + " has been updated."
 
 });
 
 export function LinkToPost(event) {
-	
+	 wixLocation.to("/request/" + currentItem._id);
 }
